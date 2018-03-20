fb4fb492765acc7fe9b1aeb72e49ea42a8279b8d
NJDB <23075@ma-web.nl>
2018-03-20 14:41:36 +0100

    UI show message
    + mapje voor main menu scenes

---
+++ Assets/Scripts/UI/ShowUI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ShowUI : MonoBehaviour {
+
+    [SerializeField]
+    Canvas messageCanvas;
+
+    void Start()
+    {
+
+        messageCanvas.enabled = false;
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "Player")
+        {
+            MessageShow();
+        }
+    }
+
+    public void MessageShow()
+    {
+        messageCanvas.enabled = true;
+    }
+
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.name == "Player")
+        {
+            MessageHide();
+        }
+    }
+
+    public void MessageHide()
+    {
+        messageCanvas.enabled = false;
+    }
+}
