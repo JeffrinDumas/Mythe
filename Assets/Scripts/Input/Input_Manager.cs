1d10d4b3eae5f06fb32c8087196bd392d50aecae
Feaxxae <duncan.westerdijk@quicknet.nl>
2018-03-20 14:31:10 +0100

    Everything I have
    This contains the first version of the tutorial level, with the buttons and doors working (pressure plate is still in progress)

---
+++ Assets/Scripts/Input/Input_Manager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script is the input manager, it manages the input given in:
+/// Edit>Project_Settings>Input
+/// </summary>
+
+public class Input_Manager : MonoBehaviour {
+
+    [SerializeField]
+    private float axisThreshhold = 0.2f;
+
+    public bool Left()
+    {
+        return Input.GetAxis("Horizontal") < -axisThreshhold;
+    }
+    public bool Right()
+    {
+        return Input.GetAxis("Horizontal") > axisThreshhold;
+    }
+    public bool Up()
+    {
+        return Input.GetAxis("Vertical") > axisThreshhold;
+    }
+    public bool Down()
+    {
+        return Input.GetAxis("Vertical") < -axisThreshhold;
+    }
+    public bool Jump()
+    {
+        return Input.GetButton("Jump");
+    }
+    public bool Action()
+    {
+        return Input.GetButton("Fire1");
+    }
+
+}
