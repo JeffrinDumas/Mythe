af19381bb7bf7707493f7aedb7497375b5da2101
NJDB <23075@ma-web.nl>
2018-04-06 13:28:46 +0200

    knopjes enzo
    nog geen Pause menu
    bijna af

--- Assets/Scripts/UI/ShowUI.cs
+++ Assets/Scripts/UI/ShowUI.cs
@@ -1,23 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class ShowUI : MonoBehaviour {
+    
 
     [SerializeField]
     Canvas messageCanvas;
 
+    
+    //Turns the canvas off at the start.
     void Start()
     {
 
         messageCanvas.enabled = false;
 
     }
-
+    //Turns canvas on when player Collides with collider
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.name == "Player")
+        if (other.tag == "Player")
         {
             MessageShow();
         }
@@ -28,10 +32,10 @@ public class ShowUI : MonoBehaviour {
         messageCanvas.enabled = true;
     }
 
-
+    //Turns canvas off when player leaves collider.
     void OnTriggerExit2D(Collider2D other)
     {
-        if (other.name == "Player")
+        if (other.tag == "Player")
         {
             MessageHide();
         }
